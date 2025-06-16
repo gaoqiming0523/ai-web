be22e7ead2b3af9e93f1d598e2e44e4589fd7677
Ge <ge@192.168.1.199>
2025-06-16 23:23:18 +0800

    fix: delete openapi apikey

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,5 @@
 export const API_CONFIG = {
     GEMINI_API_KEY: 'AIzaSyDczoD-TeHvvsG5ZuMfjEUor_DHOMnh68w', // 替换为您的 Gemini API Key
-    OPENAI_API_KEY: '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', 
   
   MODELS: {
     OPENAI: [
